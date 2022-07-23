238d2d1f396b338a1694a453f42e8c6d945f7a79
Gustavo <gustavopolonio1@gmail.com>
2022-07-23 18:21:08 -0300

    feature: responsive layout first commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import { SignInButton } from '../SignInButton'
 import { ActiveLink } from '../ActiveLink'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 import styles from './styles.module.scss'
 
 export function Header() {
+  const [isMenuHamburguerOpen, setIsMenuHamburguerOpen] = useState(false)
+
+  function toggleMenuHamburguer() {
+    setIsMenuHamburguerOpen(true)
+  }
 
   return (
     <header className={styles.headerContainer}>
@@ -11,6 +18,13 @@ export function Header() {
         <img src="/images/logo.svg" alt="ig news" />
         
         <nav>
+          <button 
+            onClick={() => toggleMenuHamburguer()} 
+            className={isMenuHamburguerOpen ? 'dsa' : styles.menuHamburguerButton}
+          >
+            <GiHamburgerMenu />
+          </button>
+
           <ActiveLink href="/" activeClassName={styles.active}>
             <a>Home</a>
           </ActiveLink>
